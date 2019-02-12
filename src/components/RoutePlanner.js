24a3d5ad4f18d62037223c13c80dd23415019fac
Scott McCormick <scott.t.mccormick@gmail.com>
2019-02-12 15:59:24 -0800

    Add Route Planner component and button to toggle visibility.

---
+++ src/components/RoutePlanner.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class RoutePlanner extends Component {
+  render() {
+    return (
+      <h2>Route Planner</h2>
+    )
+  }
+}
+
+export default RoutePlanner;
