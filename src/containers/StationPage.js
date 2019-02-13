a8440ab0b5e91b0f2a0b796ca1dad01af7887c16
Scott McCormick <scott.t.mccormick@gmail.com>
2019-02-13 10:35:04 -0800

    Temporary styling added to station show page.

--- src/containers/StationPage.js
+++ src/containers/StationPage.js
@@ -4,18 +4,16 @@ const StationPage = (props) => {
   return (
     <div>
       <h3>Current Station: {props.stationName}</h3>
-      <button onClick={props.goBack}>Go Back</button>
+      <button style={{display: "block", margin: 'auto'}} onClick={props.goBack}>Go Back</button>
       { props.etd ? 
         props.etd.map((direction, i) => {
-          return <div key={i}>
-            {direction.destination}
-              <ul>
-                {
-                  direction.estimate.map((est, j) => {
-                    return <li key={j}>{est.minutes}{est.minutes !== 'Leaving' ? ' minutes' : ''}</li>
-                  })
-                }
-              </ul>
+          return <div key={i} style={{width: '50%', display: "inline-block"}}>
+            <h4>{direction.destination}</h4>
+              {
+                direction.estimate.map((est, j) => {
+                  return <p key={j}>{est.minutes}{est.minutes !== 'Leaving' ? ' minutes' : ''}</p>
+                })
+              }
             </div>
         }) : null
       }
