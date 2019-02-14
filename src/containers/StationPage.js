fa118f3d0891b1b71ec42ec0a356d06c70f9ad4d
Scott McCormick <scott.t.mccormick@gmail.com>
2019-02-14 14:32:20 -0800

    Station page direction header margins and background colors changed.

--- src/containers/StationPage.js
+++ src/containers/StationPage.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Button, ListGroup, ListGroupItem } from 'reactstrap';
+import { Button, ListGroup, ListGroupItem, Row, Col } from 'reactstrap';
 
 const StationPage = (props) => {
   return (
     <div className="mt-2">
-      {/* <button style={{display: "block", margin: 'auto'}} onClick={props.goBack}>Go Back</button> */}
       {
         props.logged ? 
         <div>
@@ -17,21 +16,28 @@ const StationPage = (props) => {
       }
       { props.etd ? 
         props.etd.map((direction, i) => {
-          return <div key={i}  className="text-dark p-2">
-            <h5 className="text-light">{direction.destination}</h5>
-              <ListGroup className="bg-light p-3 m-3">
+          return <div key={i}  className="text-dark bg-secondary pt-2">
+            <h5 className="text-light mx-3">  
+              {direction.destination}
+            </h5>
+              <ListGroup className="p-2 mb-3 mt-1">
               {
-                direction.estimate.map((est, j) => {
-                  console.log(est)
-                  return <ListGroupItem className="my-1 text-dark d-flex justify-content-between align-items-center" style={{boxShadow: "inset 0px 0px 0px 3px " + est.hexcolor + "66"}} key={j}>
-                    {Number(est.delay) > 0 ? 
-                    <span className="badge badge-warning badge-pill mx-1">{Math.ceil(Number(est.delay) / 60)} min delay</span> :
-                    <span className="badge badge-success badge-pill mx-1">On Time</span>
-                    }
-                    
-                    <strong>{est.minutes}{est.minutes !== 'Leaving' ? ' min' : ''}</strong>
-                    <span className="badge badge-secondary badge-pill mx-1">{est.length} cars</span>
-                    
+                direction.estimate.map((est,  j) => {
+                  console.log(direction)
+                  return <ListGroupItem className="my-1 text-dark" style={{}} key={j}>
+                    <Row className="justify-content-md-center">
+                      <Col xs="4" md="3">{Number(est.delay) > 0 ? 
+                        <span className="badge badge-warning badge-pill mx-1">{Math.ceil(Number(est.delay) / 60)} min delay</span> :
+                        <span className="badge badge-success badge-pill mx-1">On Time</span>
+                        }
+                      </Col>
+                      <Col xs="4" md="3">
+                        <span>{est.minutes}{est.minutes !== 'Leaving' ? ' min' : ''}</span>
+                      </Col>
+                      <Col xs="4" md="3">
+                        <span className="badge badge-secondary badge-pill mx-1">{est.length} cars</span>
+                      </Col>
+                    </Row>
                   </ListGroupItem>
                 })
               }
