1309f4db7c484fa0e70e1b2f38c3ca496a1bdaaf
Scott McCormick <scott.t.mccormick@gmail.com>
2019-02-14 14:56:00 -0800

    Route Planner submit button disabled if illegal station choices (none or same)

--- src/components/RoutePlanner.js
+++ src/components/RoutePlanner.js
@@ -44,6 +44,13 @@ class RoutePlanner extends Component {
         console.log(err);
       })
   }
+  clearForm = () => {
+    this.setState({
+      origin: '',
+      destination: '',
+      searched: false
+    });
+  }
   componentDidMount() {
     this.setState({
       searched: false
@@ -64,8 +71,8 @@ class RoutePlanner extends Component {
     console.log(this.state)
     return (
       <div className="bg-light">
-        <RouteForm handleSubmit={this.handleSubmit} handleInput={this.handleInput} formInfo={this.state}/>
-        <button onClick={this.props.hideRoutePlanner}>Close</button>
+        <RouteForm handleSubmit={this.handleSubmit} handleInput={this.handleInput} formInfo={this.state} clearForm={this.clearForm}/>
+        {/* <button onClick={this.props.hideRoutePlanner}>Clear</button> */}
         {this.state.searched ?
           (this.state.routes.length > 0 ? 
           <RouteResults results={this.state.routes} origin={this.state.origin} destination={this.state.destination} addFavorite={this.props.addFavorite} logged={this.props.logged} favorites={this.props.favorites} deleteFavorite={this.props.deleteFavorite} /> : <p>Loading...</p>)
--- src/containers/RouteForm.js
+++ src/containers/RouteForm.js
@@ -1,29 +1,42 @@
 import React from 'react';
-import { Form, FormGroup, Label } from 'reactstrap';
+import { Form, FormGroup, Label, Button, Row, Col } from 'reactstrap';
 
 const RouteForm = (props) => {
   return (
-    <Form onSubmit={props.handleSubmit} className="bg-light">
-      <FormGroup>
-        <Label>Origin:</Label>
-        <select className="custom-select custom-select-md" name="origin" value={props.formInfo.origin} onChange={props.handleInput}>
-          <option key={-1} value="">Please choose a station</option>
-          {props.formInfo.stations.map((station, i) => {
-            return <option key={i} value={station.abbr}>{station.name}</option>
-          })}
-        </select>
-      </FormGroup>
-      <FormGroup>
-        <Label>Destination:</Label>
-        <select className="custom-select custom-select-md" name="destination" value={props.formInfo.destination} onChange={props.handleInput}>
-          <option key={-1} value="">Please choose a station</option>
-          {props.formInfo.stations.map((station, i) => {
-            return <option key={i} value={station.abbr}>{station.name}</option>
-          })}
-        </select>
-      </FormGroup>
-      <input type="submit" value="Submit"/>
-    </Form>
+    <div>
+      <Form onSubmit={props.handleSubmit} className="bg-light p-2">
+        <Row form>
+          <Col md={6}>
+            <FormGroup>
+              <Label>Origin:</Label>
+              <select className="custom-select custom-select-md" name="origin" value={props.formInfo.origin} onChange={props.handleInput}>
+                <option key={-1} value="">Please choose a station</option>
+                {props.formInfo.stations.map((station, i) => {
+                  return <option key={i} value={station.abbr}>{station.name}</option>
+                })}
+              </select>
+            </FormGroup>
+          </Col>
+          <Col md={6}>
+            <FormGroup>
+              <Label>Destination:</Label>
+              <select className="custom-select custom-select-md" name="destination" value={props.formInfo.destination} onChange={props.handleInput}>
+                <option key={-1} value="">Please choose a station</option>
+                {props.formInfo.stations.map((station, i) => {
+                  return <option key={i} value={station.abbr}>{station.name}</option>
+                })}
+              </select>
+            </FormGroup>
+          </Col>
+        </Row>
+        { props.formInfo.origin === '' || props.formInfo.destination === '' || props.formInfo.origin === props.formInfo.destination ? 
+          <Button color="success" type="submit" disabled>Submit</Button> : 
+          <Button color="success" type="submit" >Submit</Button>  }
+        
+        <Button color="light" onClick={props.clearForm}>Clear</Button>
+      </Form>
+      
+    </div>
   )
 }
 
