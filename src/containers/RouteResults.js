761edd038e06d2bc19c2b281240bee871759fbae
Scott McCormick <scott.t.mccormick@gmail.com>
2019-02-15 00:21:47 -0800

    Added padding and responsive columns to multi-leg route planner results.

--- src/containers/RouteResults.js
+++ src/containers/RouteResults.js
@@ -6,16 +6,16 @@ const RouteResults = (props) => {
     return (
       <div key={i} className="bg-secondary mb-3 py-2" >
         <CardTitle className="text-light">
-          {props.origin} => {props.destination} ({result['@tripTime']} min)
+          {result['@origin']} => {result['@destination']} ({result['@tripTime']} min)
         </CardTitle>
         <Row className="justify-content-center">
           {result.leg.map((eachLeg, j) => {
             return (
-              <Col key={j} sm={6}>
-                <Card className="p-2">
-                  <CardTitle>
+              <Col key={j} sm={result.leg.length > 1 ? 6 : 8}>
+                <Card className="p-2 my-2 mx-3">
+                  {/* <CardTitle> */}
                     {result.leg.length > 1 ? <CardTitle>Leg {j + 1}</CardTitle>	: null}
-                  </CardTitle>
+                  {/* </CardTitle> */}
                   <CardSubtitle>
                     {eachLeg['@trainHeadStation']}
                   </CardSubtitle>
